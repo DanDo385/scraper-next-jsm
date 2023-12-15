2c2c49dedbbb32c87632059ab931c78a232bdb24
Daniel Magro <dando385@yahoo.com>
2023-12-14 20:59:42 -0500

    navbar images and icons updated

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,10 +8,33 @@ const navIcons = [
 ]
 const Navbar = () => {
   return (
-    <header className = "w-full">
-        <nav className = "nav">
+    <header className="w-full">
+        <nav className='nav'>
+        <Link href="/" className="flex items-center gap-1">
+          <Image 
+            src="/assets/icons/logo.svg"
+            width={27}
+            height={27}
+            alt="logo"
+          />
 
-        </nav>
+          <p className="nav-logo">
+            Price<span className='text-primary'>Wise</span>
+          </p>
+        </Link>
+        </nav>  <div className="flex items-center gap-5">
+          {navIcons.map((icon) => (
+            <Image 
+              key={icon.alt}
+              src={icon.src}
+              alt={icon.alt}
+              width={28}
+              height={28}
+              className="object-contain"
+            />
+          ))}
+        </div>
+      </nav>
     </header>
   
   
