5ef53b95dd4fb847a7ca1fdf6f7c4f11cb368507
Daniel Magro <dando385@yahoo.com>
2023-12-18 20:10:30 -0500

    added form data

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -28,19 +28,44 @@ const Searchbar = () => {
   
   const handleSubmit = async (event: FormEvent<HTMLFormElements>) => {
     event.preventDefault();
-    const isValidLink = isValidProductURL(searchPrompt);  
-    if (!isValidLink) 
-      return ;
+    const isValidLink = isValidAmazonProductURL(searchPrompt);
+
+    if(!isValidLink) return alert('Please provide a valid Amazon link')
+
+    try {
+      setIsLoading(true);
+
+      // Scrape the product page
+      const product = await scrapeAndStoreProduct(searchPrompt);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
     }
   }
+
   return (
     <form 
-        className="flex flex-wrap gap-4 mt-12"
-        
+      className="flex flex-wrap gap-4 mt-12" 
+      onSubmit={handleSubmit}
+    >
+      <input 
+        type="text"
+        value={searchPrompt}
+        onChange={(e) => setSearchPrompt(e.target.value)}
+        placeholder="Enter product link"
+        className="searchbar-input"
+      />
+
+      <button 
+        type="submit" 
+        className="searchbar-btn"
+        disabled={searchPrompt === ''}
+      >
+        {isLoading ? 'Searching...' : 'Search'}
+      </button>
+    </form>
   )
 }
-
-
-
 
 export default Searchbar;
