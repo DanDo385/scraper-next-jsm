4cc27e420cbe94c99a2484aba76a40e4a7155bd6
Daniel Magro <dando385@yahoo.com>
2023-12-18 20:01:44 -0500

    added trycatch to scrape criteria

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -5,6 +5,22 @@ import { FormEvent, useState } from 'react';
 //Import web scraper
 import { scrapeAndStoreProduct } from '@/lib/actions';
 
+const isValidProductURL = (url : string) => {
+  try {
+    const parsedURL = new URL(url);
+    const hostname = parsedURL.hostname;
+  
+    if (hostname.includes('amazon.com') || 
+        hostname.includes('amazon.c') ||
+        hostname.includes('amazon'))
+        {
+          return true;
+        }
+  } catch {
+    return false;
+  }
+}
+
 
 const Searchbar = () => {
     const handleSubmit = () ={}
@@ -14,5 +30,8 @@ const Searchbar = () => {
         
   )
 }
+
+
+
 
 export default Searchbar;
